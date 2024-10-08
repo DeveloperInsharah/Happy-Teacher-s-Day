122b8a63c6d83624dfa52035bfb66551cc2b1f08
Insharah's Developer <insharahattariya@gmail.com>
2024-10-08 06:11:00 -0700

    For Sir Rizwan

---
+++ app.js
@@ -0,0 +1,5 @@
+window.alert("Dear Respected Sir, Sir Rizwan Happy Teacher's Day 💐💐💐");
+console.log("HAPPY TEACHERS DAY 🌷🌷🌷");
+console.log("YOU ARE THE BEST TEACHER 💙💙💙");
+console.log("MAY ALLAH BLESS YOU EVERY THING YOU WANT 🤲🏻🤲🏻🤲🏻");
+console.log("SORRY I AM LATE 😔😔😔")
